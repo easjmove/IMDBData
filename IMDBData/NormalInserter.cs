e03e5d1aafdf916d97050cd04cd9a4e005336f95
move <move@zealand.dk>
2024-09-19 14:33:43 +0200

    added normalinserter

---
+++ IMDBData/NormalInserter.cs
@@ -0,0 +1,46 @@
+﻿using IMDBData.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBData
+{
+    public class NormalInserter
+    {
+        public NormalInserter() { }
+
+        public void Insert(List<Title> titles, SqlConnection sqlConn)
+        {
+            foreach (Title title in titles)
+            {
+                string SQL = "INSERT INTO [Titles]([TConst]," +
+                    "[PrimaryTitle],[OriginalTitle],[IsAdult],[StartYear]," +
+                    "[EndYear],[RuntimeMinutes]) " +
+                    "VALUES('" + title.TConst + "'" +
+                    ",'" + title.PrimaryTitle.Replace("'","''") + "'" +
+                    ",'" + title.OriginalTitle.Replace("'", "''") + "'" +
+                    ",'" + title.IsAdult + "'" +
+                    "," + CheckIntForNull(title.StartYear) +
+                    "," + CheckIntForNull(title.EndYear) +
+                    "," + CheckIntForNull(title.RuntimeMinutes) + ")";
+
+                //throw new Exception(SQL);
+
+                SqlCommand sqlComm = new SqlCommand(SQL, sqlConn);
+                sqlComm.ExecuteNonQuery();
+            }
+        }
+
+        public string CheckIntForNull(int? value)
+        {
+            if (value == null)
+            {
+                return "NULL";
+            }
+            return value.ToString();
+        }
+    }
+}
--- IMDBData/Program.cs
+++ IMDBData/Program.cs
@@ -1,7 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+using IMDBData;
+using IMDBData.Models;
+using System.Data.SqlClient;
+
 int lineCount = 0;
+List<Title> titles = new List<Title>();
 string filePath = "C:/temp/title.basics.tsv/data.tsv";
 foreach (string line in File.ReadLines(filePath).Skip(1))
 {
@@ -14,10 +19,66 @@ foreach (string line in File.ReadLines(filePath).Skip(1))
     {
         throw new Exception("Ikke rigtigt antal tabs! " + line);
     }
+
     string tconst = splitLine[0];
     string primaryTitle = splitLine[2];
     string originalTitle = splitLine[3];
+    // kan den måske være andet end 1 og 0 ?????
     bool isAdult = splitLine[4] == "1";
+    int? startYear = ParseInt(splitLine[5]);
+    int? endYear = ParseInt(splitLine[6]);
+    int? runtimeMinutes = ParseInt(splitLine[7]);
+
+    Title newTitle = new Title()
+    {
+        TConst = tconst,
+        PrimaryTitle = primaryTitle,
+        OriginalTitle = originalTitle,
+        IsAdult = isAdult,
+        StartYear = startYear,
+        EndYear = endYear,
+        RuntimeMinutes = runtimeMinutes
+    };
+
+    titles.Add(newTitle);
 
     lineCount++;
+}
+
+Console.WriteLine("List of titles length: " + titles.Count);
+
+SqlConnection sqlConn = new SqlConnection("server=localhost;database=IMDB;" +
+    "user id=sa;password=2024forlife;TrustServerCertificate=True");
+
+sqlConn.Open();
+SqlTransaction transAction = sqlConn.BeginTransaction();
+
+DateTime before = DateTime.Now;
+
+try
+{
+    NormalInserter inserter = new NormalInserter();
+    inserter.Insert(titles, sqlConn);
+    //transAction.Commit();
+    transAction.Rollback();
+}
+catch (SqlException ex)
+{
+    Console.WriteLine(ex.Message);
+    transAction.Rollback();
+}
+
+DateTime after = DateTime.Now;
+
+sqlConn.Close();
+
+Console.WriteLine("milliseconds passed: " + (after - before).TotalMilliseconds);
+
+int? ParseInt(string value)
+{
+    if (value.ToLower() == "\\n") // checks if it is \n
+    {
+        return null;
+    }
+    return int.Parse(value);
 }
